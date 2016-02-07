25c5938c89c88f626ab6b598bfeb07089eec5a17
jonstark <nevernight721@gmail.com>
2016-02-07 18:28:16 +0500

    fdsfgdfd

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -2,7 +2,7 @@ class Lead < ActiveRecord::Base
  # before_save {  self.status = "0" if !self.status}
   mount_uploader :photo1, PhotoUploader
   mount_uploader :photo2, PhotoUploader
-  mount_uploader :status_photo, PhotoUploader
+  #mount_uploader :status_photo, PhotoUploader
 
 
   #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
