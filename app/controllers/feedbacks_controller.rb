e364a64e5992d02a0c019cd50cc4c583057b56b4
jonstark <nevernight721@gmail.com>
2016-02-13 14:50:09 +0500

    fd

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -69,6 +69,6 @@ class FeedbacksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def feedback_params
-      params.require(:feedback).permit(:k, :name, :location, :yelp_url)
+      params.require(:feedback).permit(:k, :name, :location, :yelp_url, :photo_url)
     end
 end
